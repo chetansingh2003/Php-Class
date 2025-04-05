8ef2d57655380814d07d350a095f4bd7b432a97f
Chetan Singh <71486761+chetansingh2003@users.noreply.github.com>
2025-04-05 13:49:54 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+echo 'Variable <br>';
+$Intro = "My name is Chetan";
+echo $Intro;
+echo "<br>";
+$age = "21";
+echo "I am $age year old";
+
+
+$chapri = "lover";
+echo "<br>";
+echo $chapri;
+echo "<br>";
+echo $chapri;
+echo "<br>";
+echo $chapri;
+echo "<br>";
+echo $chapri;
+
+?>
